9b7176a0b91f34b0fd29721f84c79d545aca4f3f
Lee1049 <leebusuttil@gmail.com>
2025-04-22 15:24:20 +0100

    minor code clean up

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,38 +1,7 @@
-// import { Text, View, TextInput, Button } from "react-native";
-// import { useState, useContext } from "react";
-
-// import DateTimePicker from "@react-native-community/datetimepicker"; // iOS / Android only
-// import DatePicker from "react-datepicker"; // Web only
-// import "react-datepicker/dist/react-datepicker.css"; // Web app calendar style
-// import "../datepicker-custom.css"; // Custom CSS for datepicker
-
-// export default function DatePickerComponent({ setDate }) {
-//   const [selectedDate, setSelectedDate] = useState(null);
-//   const today = new Date();
-
-//   const handleChange = (date) => {
-//     setSelectedDate(date);
-//     setDate(date);
-//   };
-//   return (
-//     <View>
-//       <DatePicker
-//         selected={selectedDate}
-//         onChange={handleChange}
-//         dateFormat="dd-MM-yyyy"
-//         placeholderText="Select a date Girly Pop!"
-//         minDate={today}
-//         popperPlacement="top-start"
-//       />
-//     </View>
-//   );
-// }
-
 import { Platform, View, Text, TouchableOpacity } from "react-native";
-import { Button } from "react-native"; // Default React Native button
 import RNDateTimePicker, {
   DateTimePickerAndroid,
-} from "@react-native-community/datetimepicker"; // Native only
+} from "@react-native-community/datetimepicker";
 
 interface DatePickerProps {
   onChange: (date: Date) => void;
