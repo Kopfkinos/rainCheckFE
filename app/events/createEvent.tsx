521e97aa277f66d5ee6c35d7beb5eba3a69cc526
Lee1049 <leebusuttil@gmail.com>
2025-04-23 11:38:38 +0100

    details added to EventDetails to show the time that has been selected. Full file is currently working in Expo Go

--- app/events/createEvent.tsx
+++ app/events/createEvent.tsx
@@ -1,83 +1,86 @@
-import { Text, View, TextInput, Button, Image, StyleSheet } from "react-native"
-import { useState, useContext } from "react"
-import { Redirect, Link } from "expo-router"
-import { useRouter } from "expo-router"
-import { postEvent } from "@/utils/api-funcs"
-import { UserContext } from "../../contexts/UserContext"
+import { Text, View, TextInput, Button, Image, StyleSheet } from "react-native";
+import { useState, useContext } from "react";
+import { Redirect, Link } from "expo-router";
+import { useRouter } from "expo-router";
+import { postEvent } from "@/utils/api-funcs";
+import { UserContext } from "../../contexts/UserContext";
 
-import LoadingUmbrella from "../../components/LoadingUmbrella"
+import LoadingUmbrella from "../../components/LoadingUmbrella";
 // POST /users/:username/events
-import DatePickerComponent from "../../components/DatePicker"
-import TimePickerComponent from "../../components/TimePicker"
+import DatePickerComponent from "../../components/DatePicker";
+import TimePickerComponent from "../../components/TimePicker";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from "react-native-responsive-screen"
+} from "react-native-responsive-screen";
 
-import "react-datepicker/dist/react-datepicker.css" // Web app calendar style
-import { SafeAreaView } from "react-native-safe-area-context"
-import { isAbsolute } from "path"
+import "react-datepicker/dist/react-datepicker.css"; // Web app calendar style
+import { SafeAreaView } from "react-native-safe-area-context";
+import { isAbsolute } from "path";
 
 export default function CreateEvent() {
   // useState here
-  const [isLoading, setIsLoading] = useState(false)
-  const [title, setTitle] = useState("")
-  const [date, setDate] = useState(new Date())
-  const [time, setTime] = useState(new Date())
-  const [location, setLocation] = useState("")
-  const [description, setDescription] = useState("")
-  const { user, setUser } = useContext(UserContext)
+  const [isLoading, setIsLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+  const [location, setLocation] = useState("");
+  const [description, setDescription] = useState("");
+  const { user, setUser } = useContext(UserContext);
   //handleSubmit here
-  const router = useRouter()
+  const router = useRouter();
 
   // Redirect if no user is logged in
   if (!user) {
-    return <Redirect href="/" />
+    return <Redirect href="/" />;
   }
 
   const handleSubmit = () => {
     if (!title || !date || !location || !description) {
-      alert("Please fill in all fields before submitting Girly Pop!")
-      return
+      alert("Please fill in all fields before submitting Girly Pop!");
+      return;
     }
     //group variables together
     const eventData = {
       title,
       description,
       date: date.toISOString().split("T")[0], // YYYY-MM-DD
-      time: time.toTimeString().split(" ")[0], // gets "HH:MM:SS"
+      time: time.toTimeString().slice(0, 8), // gets "HH:MM:SS"
       location,
       created_by: user,
       invited: null,
       host_flaked: false,
       invitee_flaked: false,
-    }
+    };
 
-    setIsLoading(true)
+    setIsLoading(true);
 
     postEvent(eventData)
       .then((newEventData) => {
         router.push({
           pathname: `/events/${newEventData.event_id}` as const,
           // 'as const' is saying 'yes TS, this is a real path'
-        } as any)
+        } as any);
       })
       .catch(() => {
-        alert("Something went wrong creating the event Girly Pop!")
+        alert("Something went wrong creating the event Girly Pop!");
       })
       .finally(() => {
-        setIsLoading(false)
-      })
-  }
+        setIsLoading(false);
+      });
+  };
 
   if (isLoading) {
-    return <LoadingUmbrella />
+    return <LoadingUmbrella />;
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.logoWrapper}>
-        <Image source={require("../../assets/images/rainCheck-logo.png")} style={styles.logo} />
+        <Image
+          source={require("../../assets/images/rainCheck-logo.png")}
+          style={styles.logo}
+        />
       </View>
       <View>
         <TextInput
@@ -112,7 +115,7 @@ export default function CreateEvent() {
         <Button title="Submit" onPress={handleSubmit} />
       </View>
     </SafeAreaView>
-  )
+  );
 }
 const styles = StyleSheet.create({
   container: {
@@ -140,4 +143,4 @@ const styles = StyleSheet.create({
     borderColor: "#ddd",
     boxShadow: "10px 4px 50px rgba(0, 0, 0, 0.1)",
   },
-})
+});
