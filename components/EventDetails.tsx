a14f4c763c12c267914864462456f141d42e7e39
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-04-23 15:04:49 +0100

    updated styling of flake button

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -29,14 +29,14 @@ export default function EventDetails({ event }) {
       <Text style={styles.italic}>📆 Date: </Text> <Text style={styles.bold}>{formattedDate}</Text>
       <Text style={styles.italic}>🕐Time:</Text> <Text style={styles.bold}>{formatedTime}</Text>
       <Text style={styles.italic}>📍Location: </Text> <Text style={styles.bold}>{location}</Text>
-      <Text style={styles.italic}>📝Description: </Text>{" "}
+      <Text style={styles.italic}>📝Description: </Text>
       <Text style={styles.bold}>{description}</Text>
       <Text style={styles.italic}>💁‍♀️Host: </Text>
       <Text style={styles.bold}>{created_by}</Text>
       {event.invited ? (
         <View>
           <Text style={styles.italic}>🙌Invited: </Text>
-          <Text style={styles.bold}>{invited}</Text>{" "}
+          <Text style={styles.bold}>{invited}</Text>
         </View>
       ) : null}
     </View>
@@ -56,12 +56,12 @@ const styles = StyleSheet.create({
   imageContainer: {},
   eventImage: {
     alignSelf: "center",
-    width: wp("75%"),
-    height: 150,
+    width: wp("85%"),
+    height: hp("25%"),
     borderRadius: 100 / 2,
     overflow: "hidden",
     borderWidth: 3,
-    borderColor: "#6675D7",
+    borderColor: "white",
     marginBottom: 10,
   },
 })
