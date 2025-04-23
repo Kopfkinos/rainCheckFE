0ce00e1e8b85ed87e7968d74812d4c2d69252191
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-04-23 10:28:13 +0100

    update both flaked button

--- components/BothFlaked.tsx
+++ components/BothFlaked.tsx
@@ -1,9 +1,11 @@
 import { View, Image, Text, StyleSheet } from "react-native"
 
-export default function BothFlaked() {
+export default function BothFlaked({ eventTitle }) {
+  console.log(eventTitle)
   return (
     <View style={styles.logoWrapper}>
       <Image source={require("../assets/images/rainCheck-logo.png")} style={styles.logo} />
+      <Text style={styles.eventTitle}>{eventTitle}</Text>
       <Image source={require("../assets/images/both-flaked.gif")} style={styles.gif} />
       <Text style={styles.header}> You're both not feeling it!</Text>
       <Text> Great minds flake alike... </Text>
@@ -28,5 +30,11 @@ const styles = StyleSheet.create({
   header: {
     margin: 25,
     fontSize: 25,
+  },
+  eventTitle: {
+    color: "black",
+    fontSize: 24,
+    marginBottom: 20,
+    alignSelf: "center",
   },
 })
