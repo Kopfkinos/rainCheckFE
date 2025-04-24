36ece877b1caf970e129d4556dc812f224d72644
IgelIntelligenz <connor_v_d_murphy@hotmail.com>
2025-04-24 12:24:31 +0100

    update event page styling

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -18,85 +18,89 @@ export default function EventDetails({ event }) {
   const formatedTime = time.slice(0, 5)
 
   return (
-		<View>
-			<View style={styles.imageContainer}>
-				<Image
-					style={styles.eventImage}
-					source={event_img_url ? { uri: event_img_url } : require("../assets/images/app-icon.png")}
-				/>
-			</View>
-			<Text style={styles.heading}>{title}</Text>
+    <View>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.eventImage}
+          source={event_img_url ? { uri: event_img_url } : require("../assets/images/app-icon.png")}
+        />
+      </View>
+      <Text style={styles.heading}>{title}</Text>
 
-			<View style={styles.columnsContainer}>
-				<View style={styles.column}>
-					<Text style={styles.date}>
-						<Text style={styles.italic}>📆 Date: </Text>{" "}
-						<Text style={styles.bold}>{formattedDate}</Text>{" "}
-					</Text>
+      <View style={styles.columnsContainer}>
+        <View style={styles.column}>
+          <Text style={styles.date}>
+            <Text style={styles.italic}>📆 Date: </Text>{" "}
+            <Text style={styles.bold}>{formattedDate}</Text>{" "}
+          </Text>
 
-					<Text>
-						<Text style={styles.italic}>🕐Time:</Text>{" "}
-						<Text style={styles.bold}>{formatedTime}</Text>{" "}
-					</Text>
+          <Text>
+            <Text style={styles.italic}>🕐Time:</Text>{" "}
+            <Text style={styles.bold}>{formatedTime}</Text>{" "}
+          </Text>
 
-					<Text>
-						<Text style={styles.italic}>📍Location: </Text>{" "}
-						<Text style={styles.bold}>{location}</Text>{" "}
-					</Text>
-				</View>
+          <Text>
+            <Text style={styles.italic}>📍Location: </Text>{" "}
+            <Text style={styles.bold}>{location}</Text>{" "}
+          </Text>
+        </View>
 
-				<View style={styles.column}>
-					<Text>
-						<Text style={styles.italic}>💁‍♀️Host: </Text>
-						<Text style={styles.bold}>{created_by}</Text>
-					</Text>
-					{event.invited ? (
-						<View>
-							<Text style={styles.italic}>🙌Invited: </Text>
-							<Text style={styles.bold}>{invited}</Text>
-						</View>
-					) : null}
-					<Text style={styles.description}>
-						<Text style={styles.italic}>📝Description: </Text>
-						<Text style={styles.bold}>{description}</Text>
-					</Text>
-				</View>
-			</View>
-		</View>
-	);
+        <View style={styles.column}>
+          <Text>
+            <Text style={styles.italic}>💁‍♀️Host: </Text>
+            <Text style={styles.bold}>{created_by}</Text>
+          </Text>
+          {event.invited ? (
+            <View>
+              <Text style={styles.italic}>🙌Invited: </Text>
+              <Text style={styles.bold}>{invited}</Text>
+            </View>
+          ) : null}
+          <Text style={styles.description}>
+            <Text style={styles.italic}>📝Description: </Text>
+            <Text style={styles.bold}>{description}</Text>
+          </Text>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-	heading: {
-		color: "#5B3EC6",
-		fontSize: 30,
-		marginBottom: 20,
-		alignSelf: "center",
-	},
-	bold: { fontWeight: "bold" },
-	italic: { fontStyle: "italic" },
-	underline: { textDecorationLine: "underline" },
-	imageContainer: {},
-	eventImage: {
-		alignSelf: "center",
-		width: wp("85%"),
-		height: hp("25%"),
-		borderRadius: 100 / 2,
-		overflow: "hidden",
-		borderWidth: 3,
-		borderColor: "white",
-		marginTop: -10,
-		marginBottom: 10,
-		resizeMode: "contain",
-	},
-	columnsContainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		paddingHorizontal: 20,
-		marginTop: 10,
-		gap: 10,
-	},
-	column: {
-		flex: 1,
-	},
-});
+  heading: {
+    alignSelf: "center",
+    color: "#5B3EC6",
+    fontSize: 40,
+    textShadowColor: "white",
+    textShadowRadius: 5,
+    textShadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  bold: { fontWeight: "bold" },
+  italic: { fontStyle: "italic" },
+  underline: { textDecorationLine: "underline" },
+  imageContainer: {},
+  eventImage: {
+    alignSelf: "center",
+    width: wp("85%"),
+    height: hp("25%"),
+    borderRadius: 100 / 2,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: "white",
+    marginTop: -10,
+    marginBottom: 10,
+  },
+  columnsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    marginTop: 10,
+    gap: 10,
+  },
+  column: {
+    flex: 1,
+  },
+})
