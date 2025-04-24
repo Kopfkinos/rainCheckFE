36200df1b6b20ecdb0daa2405b99291a45de3817
stepharinaa <stephanielplee@gmail.com>
2025-04-24 08:32:23 +0100

    styling for ios add event page

--- app/events/createEvent.tsx
+++ app/events/createEvent.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TextInput, Button, Image, StyleSheet, TouchableOpacity } from "react-native"
+import { Text, View, TextInput, Button, Image, StyleSheet, TouchableOpacity, ImageBackground } from "react-native"
 
 import { useState, useContext } from "react"
 import { Redirect, Link } from "expo-router"
@@ -84,57 +84,104 @@ export default function CreateEvent() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.logoWrapper}>
-        <Image source={require("../../assets/images/rainCheck-logo.png")} style={styles.logo} />
-      </View>
-      <View>
-        <TextInput
-          placeholder="Enter Event Title"
-          style={styles.input}
-          value={title}
-          onChangeText={setTitle}
-        />
-
-        <View style={{ overflow: "visible", zIndex: 1 }}>
-          <DatePickerComponent onChange={setDate} currentDate={date} />
+    <ImageBackground
+      source={require("../../assets/images/homepage-bg.jpg")}
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView style={styles.container}>
+        <View style={styles.logoWrapper}>
+          <Image source={require("../../assets/images/rainCheck-logo.png")} style={styles.logo} />
         </View>
+        <View>
+          <Text style={styles.textTitle}>Create An Event</Text>
+          <TextInput
+            placeholder="Enter Event Title"
+            style={styles.input}
+            value={title}
+            onChangeText={setTitle}
+            placeholderTextColor="black"
+          />
 
-        <View style={{ overflow: "visible", zIndex: 1 }}>
-          <TimePickerComponent onChange={setTime} currentTime={time} />
+          <View style={styles.datePickerComponent}>
+            <DatePickerComponent onChange={setDate} currentDate={date} />
+          </View>
+
+          <View style={{ overflow: "visible", zIndex: 1 }}>
+            <TimePickerComponent onChange={setTime} currentTime={time} />
+          </View>
+
+          <TextInput
+            placeholder="Enter Event Location"
+            style={styles.input}
+            value={location}
+            onChangeText={setLocation}
+            placeholderTextColor="black"
+          />
+
+          <TextInput
+            placeholder="Enter Event Description"
+            style={[styles.descriptionInput, { textAlignVertical: "center" }]}
+            value={description}
+            onChangeText={setDescription}
+            multiline={true}
+            placeholderTextColor="black"
+          />
+          <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+            <Text style={styles.submitButtonText}>Submit</Text>
+          </TouchableOpacity>
         </View>
-
-        <TextInput
-          placeholder="Enter Event Location"
-          style={styles.input}
-          value={location}
-          onChangeText={setLocation}
-        />
-
-        <TextInput
-          placeholder="Enter Event Description"
-          style={styles.input}
-          value={description}
-          onChangeText={setDescription}
-          multiline={true}
-        />
-        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
-          <Text style={styles.submitButtonText}>Submit</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground >
   )
 }
 const styles = StyleSheet.create({
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 0,
+    margin: 0,
+    resizeMode: "stretch",
+  },
   container: {
     flex: 1,
     justifyContent: "center",
-    padding: 20,
-    backgroundColor: "F7F7F7",
+    resizeMode: "stretch",
+  },
+  descriptionInput: {
+    height: hp("7%"),
+    width: "100%",
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 5,
+    borderRadius: 5,
+    borderColor: "#ddd",
+    boxShadow: "10px 4px 50px rgba(0, 0, 0, 0.49)",
+    backgroundColor: "rgba(255, 255, 255, 0.34)",
+    color: "black",
+    textAlignVertical: "center",
+    paddingTop: 20,
+  },
+  textTitle: {
+    fontSize: 30,
+    textAlign: "center",
+    fontWeight: "bold",
+    marginBottom: 15,
+    color: "#fff",
+    fontFamily: "Bestime",
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: 3, height: 1 },
+    textShadowRadius: 2,
+  },
+  datePickerComponent: {
+    overflow: "visible",
+    zIndex: 1,
   },
   logoWrapper: {
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 5,
   },
   logo: {
     width: 300,
@@ -146,12 +193,13 @@ const styles = StyleSheet.create({
     width: "100%",
     marginVertical: hp("1%"),
     borderWidth: 1,
-    padding: wp("2.5%"),
+    padding: 10,
     borderRadius: 5,
     borderColor: "#ddd",
-    boxShadow: "10px 4px 50px rgba(0, 0, 0, 0.1)",
+    boxShadow: "10px 4px 50px rgba(0, 0, 0, 0.49)",
+    backgroundColor: "rgba(255, 255, 255, 0.34)",
+    color: "black",
   },
-
   submitButton: {
     backgroundColor: "#402B8B",
     paddingVertical: 14,
